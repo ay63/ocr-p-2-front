105d3bef210ec1b8eb497a6935ef8a2489c11cc3
ay63 <aym.aw36@gmail.com>
2024-12-28 15:29:42 +0100

    Feat: add countryDetailChartComponent

---
+++ src/app/pages/country-detail-chart/country-detail-chart.component.ts
@@ -0,0 +1,27 @@
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {NgxChartsModule} from "@swimlane/ngx-charts";
+import {CountryDetailChartFormat} from "../../core/types/CountryDetailChartFormat";
+
+@Component({
+  selector: 'app-country-detail-chart',
+  standalone: true,
+  imports: [NgxChartsModule],
+  templateUrl: './country-detail-chart.component.html',
+  styleUrl: './country-detail-chart.component.scss'
+})
+export class CountryDetailChartComponent {
+
+  @ViewChild('containerRef')
+  containerRef!: ElementRef;
+
+  @Input()
+  data!: CountryDetailChartFormat|[]
+
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Dates';
+  timeline: boolean = true;
+}
+
